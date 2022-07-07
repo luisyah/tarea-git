19f17054ce695a44029c9f92b20041da94222ff6
Luis Yah <luisyahmendez@gmail.com>
2022-07-06 21:10:04 -0500

    Actualizacion 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
                                 </div>
                             </div>
                         </div>
-<!--
+
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
@@ -125,7 +125,7 @@
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         
-                    </div>-->
+                    </div>
                 </div>
             </div>
         </div>
